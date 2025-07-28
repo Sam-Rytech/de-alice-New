0260f53adcc26d7e6118c020676023251cb178a5
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-07-28 04:57:39 +0100

    Updated FAQ

--- components/Faq.jsx
+++ components/Faq.jsx
@@ -2,10 +2,11 @@ import Pretitle from './Pretitle'
 import { motion } from 'framer-motion'
 import { fadeIn } from '../variants'
 import { Description } from '@radix-ui/react-dialog'
+import FaqItem from './FaqItem'
 
 const faqItemData = [
   {
-    title: 'How long does a contrustion usualyy take?',
+    title: 'How long does a contrustion usually take?',
     description: 'Timeline vary based on project size and complexity',
   },
   {
@@ -30,8 +31,42 @@ const faqItemData = [
   },
 ]
 
+const faqItemVariants = {
+  hidden: { opacity: 0, y: 30 },
+
+  visible: (index) => ({
+    opacity: 1,
+    y: 0,
+    transition: { delay: (index = 0.1), duration: 0.3 },
+  }),
+}
+
 const Faq = () => {
-  return <div>Faq</div>
+  return (
+    <section className="pt-16 xl:pt-32 px-4">
+      <div className="container mx-auto">
+        {/* text */}
+        <div className="text-center max-w-[540px] mx-auto xl:mb-20">
+          <Pretitle text="Faq" center />
+          <h2 className="h2 mb-3">Got Questions? We've Got You Covered</h2>
+          <p className="mb-11 max-w-[480px] mx-auto">
+            From project planning to final touches, we've answered the most
+            common questions to help you make informed decisions
+          </p>
+        </div>
+        {/* faq items */}
+        <ul>
+          {faqItemData.map((item, index) => {
+            return (
+              <li key={index}>
+                <FaqItem title={item.title} description={item.description}/>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    </section>
+  )
 }
 
 export default Faq

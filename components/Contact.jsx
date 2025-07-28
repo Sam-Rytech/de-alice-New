2aca280c8fee51e89f3a9bd7bb6713bf2aa5e687
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-07-28 23:45:25 +0100

    Added imports

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,6 +1,16 @@
+import { RiChat1Line, RiMap2Line, RiSmartphoneLine } from "react-icons/ri";
+import Socials from "./Socials";
+
 const Contact = () => {
   return (
-    <div id="contact" className="pt-16 xl:pt-32">contact</div>
+    <div id="contact" className="pt-16 xl:pt-32">
+      <div className="container mx-auto">
+        {/* info */}
+        <div>info</div>
+        {/* form */}
+        <div></div>
+      </div>
+    </div>
   )
 }
 

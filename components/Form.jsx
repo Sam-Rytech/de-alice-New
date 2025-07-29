f7354f531cf07c0aef1de964e40280750860776a
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-07-29 17:17:42 +0100

    Updated the classname in the dependencies

--- components/Form.jsx
+++ components/Form.jsx
@@ -10,24 +10,24 @@ import {
 
 import { Input } from './ui/input'
 import { Textarea } from './ui/textarea'
-import { Button } from './Button'
+import Button from './Button'
 
 const Form = () => {
   return (
-    <form>
-      <div>
+    <form className='flex flex-col'>
+      <div className='flex flex-col gap-[20px] mb-[20px]'>
         <Input type="fullname" placeholder="Full Name" />
         <Input type="email" placeholder="Email address" />
         <div>
           <Input type="phone" placeholder="Phone number" />
           <Select>
-            <SelectTrigger className="w-full rounded-none h-[54px] text-black outline-none">
+            <SelectTrigger className="w-full rounded-none h-[54px] secondary outline-none">
               <SelectValue placeholder="Select a Service"></SelectValue>
             </SelectTrigger>
             <SelectContent>
               <SelectGroup>
                 <SelectLabel>Select a service</SelectLabel>
-                <SelectItem value="construction">Constructiom</SelectItem>
+                <SelectItem value="construction">Construction</SelectItem>
                 <SelectItem value="renovation">Renovation</SelectItem>
                 <SelectItem value="restoration">Restoration</SelectItem>
                 <SelectItem value="consulting">Consulting</SelectItem>
@@ -38,7 +38,11 @@ const Form = () => {
       </div>
       <div>
         {/* text Area */}
-        <Textarea/>
+        <Textarea className="h-[180px] resize-none text-[#666666] rounded-none" 
+          placeholder="Enter your message"
+        />
+        {/* btn */}
+        <Button text="Send message"/>
       </div>
     </form>
   )

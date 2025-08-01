e1eac39050c3faef3fd2e5f46c2b0bbcf4793910
Omolayo Temitayo <temitayoomolayo@gmail.com>
2025-08-01 22:55:25 +0100

    Framer motion updated

--- components/Faq.jsx
+++ components/Faq.jsx
@@ -46,22 +46,35 @@ const Faq = () => {
     <section className="pt-16 xl:pt-32 px-4">
       <div className="container mx-auto">
         {/* text */}
-        <div className="text-center max-w-[540px] mx-auto xl:mb-20">
+        <motion.div
+          variants={fadeIn('up', 0.2)}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: false, amount: 0.2 }}
+          className="text-center max-w-[540px] mx-auto xl:mb-20"
+        >
           <Pretitle text="Faq" center />
           <h2 className="h2 mb-3">Got Questions? We've Got You Covered</h2>
           <p className="mb-11 max-w-[480px] mx-auto">
             From project planning to final touches, we've answered the most
             common questions to help you make informed decisions
           </p>
-        </div>
+        </motion.div>
         {/* faq items */}
-        <ul className='w-full flex flex-col'>
+        <ul className="w-full flex flex-col">
           {faqItemData.map((item, index) => {
             return (
-              <li key={index}>
+              <motion.li
+                key={index}
+                variants={faqItemVariants}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: false, amount: 0.8 }}
+                custom={index}
+              >
                 <FaqItem title={item.title} description={item.description} />
-              </li>
-            );
+              </motion.li>
+            )
           })}
         </ul>
       </div>
